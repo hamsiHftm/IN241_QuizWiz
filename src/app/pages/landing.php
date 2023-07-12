4742084d1996e92290e234520253d07eb421364d
hamsiHftm <hamsiga.rajaratnam@hftm.ch>
2023-07-12 16:39:48 +0200

    images and logo added landing page added

---
+++ src/app/pages/landing.php
@@ -0,0 +1,20 @@
+<div class="container-fluid landing-page-container">
+    <div class="row landing-page-container">
+        <div class="col-5">
+            <img src="../assets/img/quiz-wiz-logo-with-slogan.png">
+        </div>
+        <div class="col-7 blue-background-container">
+            <div>
+                <div>
+                    <p>
+                        "Unleash your inner quiz champion and embark on an exhilarating journey of knowledge. Join Quiz Wiz today and let your curiosity guide you to new horizons through the excitement of quizzing!"
+                    </p>
+                    <button class="quiz-wiz-red-button">
+                        Get Started!
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
--- src/index.php
+++ src/index.php
@@ -1,52 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>QuizWiz</title>
+    <link rel="icon" href="app/assets/img/logo_icon.svg">
+    <!-- Adding bootstrap library -->
+    <link rel="stylesheet" href="app/assets/bootstrap/css/bootstrap.css">
+    <script src="app/assets/bootstrap/js/bootstrap.js"></script>
+    <!-- Custom stylesheet -->
+    <link rel="stylesheet" href="app/assets/css/quizWiz.css">
+</head>
+<body>
+
+
 <?php
-include 'config.php';
-include 'app/models/Category.php';
-include 'app/models/Difficulty.php';
-require_once 'app/services/QuizWizDB.php';
-require_once 'app/services/OpenTriviaAPI.php';
 
-// Database connection parameters
-$host = $GLOBALS['DB_HOST'];
-$port = $GLOBALS['DB_PORT'];
-$dbname = $GLOBALS['DB_NAME'];
-$user = $GLOBALS['DB_USERNAME'];
-$password = $GLOBALS['DB_PASSWORD'];
+$showLandingPage = true;
+if ($showLandingPage) {
+    include 'app/pages/landing.php';
+} else {
 
-$api_base_url = $GLOBALS['API_BASE_URL'];
+    include 'config.php';
+    include 'app/models/Category.php';
+    include 'app/models/Difficulty.php';
+    require_once 'app/services/QuizWizDB.php';
+    require_once 'app/services/OpenTriviaAPI.php';
 
-$database = new QuizWizDB($host, $port, $dbname, $user, $password);
-$pdo = $database->getConnection();
+    // Database connection parameters
+    $host = $GLOBALS['DB_HOST'];
+    $port = $GLOBALS['DB_PORT'];
+    $dbname = $GLOBALS['DB_NAME'];
+    $user = $GLOBALS['DB_USERNAME'];
+    $password = $GLOBALS['DB_PASSWORD'];
 
-$triviaAPI = new OpenTriviaAPI($api_base_url, null);
-$category_list = $triviaAPI->getCategories();
+    $api_base_url = $GLOBALS['API_BASE_URL'];
 
-$quiz = $triviaAPI->getQuestions(10, new Category(24, "Politics"), Difficulty::EASY->value);
-foreach ($quiz->getQuestions() as $question) {
-    echo "jeei";
-    echo $question->getQuestionText();
+    $database = new QuizWizDB($host, $port, $dbname, $user, $password);
+    $pdo = $database->getConnection();
+
+    $triviaAPI = new OpenTriviaAPI($api_base_url, null);
+    $category_list = $triviaAPI->getCategories();
+
+    $quiz = $triviaAPI->getQuestions(10, new Category(24, "Politics"), Difficulty::EASY->value);
+    foreach ($quiz->getQuestions() as $question) {
+        echo "jeei";
+        echo $question->getQuestionText();
+    }
+
+    //
+    //// Retrieve data from the "user" table
+    //try {
+    //    $stmt = $pdo->query("SELECT * FROM \"player\"");
+    //    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    //} catch (PDOException $e) {
+    //    die("Error retrieving data from table: " . $e->getMessage());
+    //}
+    //
+    //// Display the data
+    //echo "<h1>User List</h1>";
+    //echo "<table>";
+    //echo "<tr><th>Name</th><th>Last Name</th></tr>";
+    //
+    //foreach ($users as $user) {
+    //    echo "<tr>";
+    //    echo "<td>" . $user['name'] . "</td>";
+    //    echo "<td>" . $user['last_name'] . "</td>";
+    //    echo "</tr>";
+    //}
+    //
+    //echo "</table>";
 }
 
-//
-//// Retrieve data from the "user" table
-//try {
-//    $stmt = $pdo->query("SELECT * FROM \"player\"");
-//    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//} catch (PDOException $e) {
-//    die("Error retrieving data from table: " . $e->getMessage());
-//}
-//
-//// Display the data
-//echo "<h1>User List</h1>";
-//echo "<table>";
-//echo "<tr><th>Name</th><th>Last Name</th></tr>";
-//
-//foreach ($users as $user) {
-//    echo "<tr>";
-//    echo "<td>" . $user['name'] . "</td>";
-//    echo "<td>" . $user['last_name'] . "</td>";
-//    echo "</tr>";
-//}
-//
-//echo "</table>";
 
 ?>
+</body>
+</html>
