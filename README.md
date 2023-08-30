566826a7b533a651d3130b670c15ae134a30d9e9
Hamsiga Rajaratnam <hamsiga.rajaratnam@ionesoft.ch>
2023-08-30 14:21:40 +0200

    readme.md updated

--- README.md
+++ README.md
@@ -1,30 +1,51 @@
 # Quiz Wiz Project
+Welcome to the Quiz Wiz project setup guide. This project is designed to run using Docker and consists of two containers: one for PHP and another for PostgreSQL. Below are the steps to set up and run the Quiz Wiz project on your local machine.  
 
-This is the Quiz Wiz project, designed to run with Docker. It consists of two containers: one for PHP and another for PostgreSQL.
+### Prerequisites
+Before you begin, make sure you have the following software installed:
+- Docker
+- PgAdmin (to manage the PostgreSQL database)
 
-**Note:** Before starting the project, make sure to create the necessary database tables. You can do this by running the following command:
+### Clone the Repository
+1. Clone this repository to your local machine using Git:  
+```git clone <repository_url>```
+2. Navigate to the project directory:  
+```cd Quiz-Wiz-Project```
 
-TODO command
-create
+### Start the Project
+1. Ensure that Docker is installed and running on your machine. 
+2. In the project directory, you will find the docker-compose.yml file. 
+3. Start the project by running the following Docker Compose command:  
+```docker-compose up -d```  
+   This command will start the containers in detached mode (-d), allowing them to run in the background.
 
-Please refer to the project documentation for detailed instructions on setting up and running the Quiz Wiz project.
+### Create Database Tables
+1. After starting the project, you need to create the required database tables. The SQL queries for creating these tables can be found in the file ```resources/sql_scripts/tables_create.sql```.
+2. Open PgAdmin to manage the PostgreSQL database.
+3. Register a new server in PgAdmin with the following configurations:
+```
+        Name: quizwiz-db
+        Hostname: localhost
+        Port: 5432
+        Maintenance Database: quizWiz
+        Username: quizWiz
+        Password: 12345
+```
+4. Once the server is registered, execute the SQL queries from ```tables_create.sql``` using PgAdmin to create the necessary database tables.
 
-Getting Started
+    **Note:** You only need to run these queries the first time you set up the project or if you delete the tables from the database.
 
-To get started with the Quiz Wiz project, follow the steps below:
+### Access the Application
+With the containers up and the tables created, you can access the Quiz Wiz application in your web browser at http://localhost:8000.
 
-1. Clone this repository to your local machine. 
-2. Ensure that Docker is installed and running. 
-3. Navigate to the project directory. 
-4. Start the project by running the Docker Compose command: docker-compose up -d. 
-5. After starting the project, execute the create command to create the required database tables. 
-6. Access the Quiz Wiz application in your web browser at http://localhost:8000.
-
-For more detailed instructions and additional information, please refer to the project documentation.
-License
-
-This project is licensed under the MIT License.
+### TODO -> Example data create and login informations 
 
 
-https://www.codeproject.com/Articles/759094/Step-by-Step-PHP-Tutorials-for-Beginners-Creating
+### Additional Information
+For more detailed instructions and further information, please consult the project documentation. This project is licensed under the MIT License.  
+
+Please remember that this guide is a simplified version. Refer to the original project documentation for comprehensive instructions and troubleshooting tips.
+
+Reference: Original Documentation Link
+
 
--- src/pages/register.php
+++ src/pages/register.php
@@ -1,6 +1,3 @@
-<?php
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -38,17 +35,17 @@
                 </div>
                 <div class="col-6" style="padding: 2em">
                     <h2>SIGNUP</h2>
-                    <form class="qw-form col d-flex flex-column justify-content-between">
+                    <form action="register.php" method="post" class="qw-form col d-flex flex-column justify-content-between">
                         <div class="form-group">
                             <label for="nameInput">Name</label>
-                            <input type="text" class="form-control" id="nameInput" aria-describedby="nameInputHelp" placeholder="Enter name">
+                            <input type="text" class="form-control" id="nameInput" name="name" aria-describedby="nameInputHelp" placeholder="Enter name">
                             <small id="nameInputHelp" class="form-text text-muted">Enter your full name. (eg. Hans Muster)</small>
                         </div>
 
                         <div class="form-group">
                             <label for="dateOfBirthInput">Date of Birth</label>
                             <div class='input-group date' id='dateOfBirthInput'>
-                                <input type='date' class="form-control" />
+                                <input type='date' class="form-control" name="dateOfBirth"/>
                                 <span class="input-group-addon">
                                     <span class="glyphicon glyphicon-calendar"></span>
                                 </span>
@@ -56,18 +53,18 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="usernameInput">Username / Email</label>
-                            <input type="text" class="form-control" id="usernameInput" aria-describedby="usernameInput" placeholder="Username/Email">
+                            <label for="usernameInput">Username / Email *</label>
+                            <input type="text" class="form-control" id="usernameInput" name="username" aria-describedby="usernameInput" placeholder="Username/Email" required>
                             <small id="usernameInput" class="form-text text-muted">You can enter your username or email</small>
                         </div>
 
                         <div class="form-group">
-                            <label for="passwordInput">Password</label>
-                            <input type="password" class="form-control" id="passwordInput" placeholder="Password">
+                            <label for="passwordInput">Password *</label>
+                            <input type="password" class="form-control" id="passwordInput" name="password" placeholder="Password" required>
                         </div>
                         <div class="form-group">
-                            <label for="repeatPasswordInput">Repeat Password</label>
-                            <input type="password" class="form-control" id="repeatPasswordInput" placeholder="Repeat Password">
+                            <label for="repeatPasswordInput">Repeat Password *</label>
+                            <input type="password" class="form-control" id="repeatPasswordInput" name="repeatPassword" placeholder="Repeat Password" required>
                         </div>
                         <div>
                             <button class="qw-red-button" type="submit">Register</button>
@@ -79,3 +76,41 @@
     </div>
 </body>
 </html>
+
+<?php
+
+require_once '../app/services/QuizWizDB.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve user-inputted values
+    $name = $_POST["name"];
+    $dateOfBirth = $_POST["dateOfBirth"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $repeatPassword = $_POST["repeatPassword"];
+
+    // Check if passwords match
+    if ($password !== $repeatPassword) {
+        // TODO add alert for invalid password
+        echo '<div class="alert alert-danger" role="alert">';
+        echo 'Passwords do not match!';
+        echo '</div>';
+    } else {
+
+        // Passwords match, continue processing
+
+        // Now you can use these variables as needed, for example, to perform database operations or validation.
+
+        // For demonstration purposes, let's just print the values:
+        echo "Name: " . $name . "<br>";
+        echo "Date of Birth: " . $dateOfBirth . "<br>";
+        echo "Username / Email: " . $username . "<br>";
+        echo "Password: " . $password . "<br>";
+        echo "Repeat Password: " . $repeatPassword . "<br>";
+    }
+
+    exit;
+}
+
+
+?>
