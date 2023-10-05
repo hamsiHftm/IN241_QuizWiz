5dbc535850d5e53ad0eda86d77d92f920000ce54
Hamsiga Rajaratnam <hamsiga.rajaratnam@ionesoft.ch>
2023-10-05 09:36:06 +0200

    deleting quiz from session when saved

--- src/app/pages/HomePage.php
+++ src/app/pages/HomePage.php
@@ -1,6 +1,7 @@
 <?php
      require_once '../controllers/AuthController.php';
      require_once '../controllers/DBController.php';
+
     // Start the session if not already started
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
